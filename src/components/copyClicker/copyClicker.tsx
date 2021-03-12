dc92d11a1f3a19cd8de44688520769e619194312
seamussmith <student.141504@worcesterschools.net>
2021-03-12 09:34:53 -0500

    Roughly implmenting the CopyClicker

--- src/components/copyClicker/copyClicker.tsx
+++ src/components/copyClicker/copyClicker.tsx
@@ -1,6 +1,27 @@
 import React from 'react'
 
-class CopyClicker extends React.Component<{},{}>
+type CopyClicker_props = {
+    copy_txt: string
+}
+type CopyClicker_state = {
+    hover_txt: string
+}
+class CopyClicker extends React.Component<CopyClicker_props, CopyClicker_state>
 {
-    
+    constructor(props: CopyClicker_props)
+    {
+        super(props)
+    }
+    render()
+    {
+        return (
+            <span
+            className="search__share-result tooltip"
+            data-hover-txt="Click to copy"
+            onClick={function(){}}
+            >
+                {/* ${document.location.origin + document.location.pathname}?pkmn=${result.name} */}
+            </span>
+        )
+    }
 }
