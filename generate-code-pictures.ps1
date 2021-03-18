7d8173e083bafa460d68a5e0523a87d39c29799d
seamussmith <student.141504@worcesterschools.net>
2021-03-18 19:09:57 -0400

    Create JSON file for carbon theme

--- generate-code-pictures.ps1
+++ generate-code-pictures.ps1
@@ -7,11 +7,11 @@ if (-not (Test-Path -Path "./generated-code-pictures"))
 cd generated-code-pictures
 
 Get-ChildItem -Path "../src" -Filter "*.tsx" -Recurse -File | ForEach-Object {
-    carbon-now $_.FullName -p csp-pic
+    carbon-now $_.FullName -p csp-pic -t ($_.BaseName+$_.Extension)
 }
 
 Get-ChildItem -Path "../src" -Filter "*.css" -Recurse -File | ForEach-Object {
-    carbon-now $_.FullName -p csp-pic
+    carbon-now $_.FullName -p csp-pic -t ($_.BaseName+$_.Extension)
 }
 
 cd ..
