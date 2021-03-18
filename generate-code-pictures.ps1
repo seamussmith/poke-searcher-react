13f4ad457719afac7e25354ee733653d6be1bb21
seamussmith <student.141504@worcesterschools.net>
2021-03-18 19:55:21 -0400

    Add document generation to gen script

--- generate-code-pictures.ps1
+++ generate-code-pictures.ps1
@@ -1,4 +1,12 @@
 
+$CodePicTemplate = @'
+
+# {0}
+
+![]({1})
+
+'@
+
 if (-not (Test-Path -Path "./generated-code-pictures"))
 {
     New-Item -ItemType directory -Path "./generated-code-pictures"
@@ -13,5 +21,15 @@ Get-ChildItem -Path "../src" -Filter "*.tsx" -Recurse -File | ForEach-Object {
 Get-ChildItem -Path "../src" -Filter "*.css" -Recurse -File | ForEach-Object {
     carbon-now $_.FullName -p csp-pic -t ($_.BaseName+$_.Extension)
 }
+
+$MarkdownFile = ""
+
+Get-Childitem -Path "." -Filter "*.png" -File | ForEach-Object {
+    $MarkdownFile = $MarkdownFile + [string]::Format($CodePicTemplate, $_.BaseName+$_.Extension, $_.BaseName+$_.Extension)
+}
+
+Out-File -FilePath .\Document.md -InputObject $MarkdownFile -Encoding utf8
+
+pandoc -o Document.docx -f markdown -t docx Document.md
 
 cd ..
