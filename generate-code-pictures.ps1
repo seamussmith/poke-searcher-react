95c9c0f2d2d0ea33c972cf7c97348ed50fb7866a
seamussmith <student.141504@worcesterschools.net>
2021-03-19 10:44:47 -0400

    Split images into 25 line segments *can cause invalid syntax hightlighting, but screw collegeboard...

--- generate-code-pictures.ps1
+++ generate-code-pictures.ps1
@@ -7,6 +7,12 @@ $CodePicTemplate = @'
 
 '@
 
+$AdditionalPic = @'
+
+![]({0})
+
+'@
+
 if (-not (Test-Path -Path "./generated-code-pictures"))
 {
     New-Item -ItemType directory -Path "./generated-code-pictures"
@@ -15,17 +21,35 @@ if (-not (Test-Path -Path "./generated-code-pictures"))
 cd generated-code-pictures
 
 Get-ChildItem -Path "../src" -Filter "*.tsx" -Recurse -File | ForEach-Object {
-    carbon-now $_.FullName -p csp-pic -t ($_.BaseName+$_.Extension)
+    $lines = [Math]::Ceiling((Get-Content $_.FullName | Measure-Object -Line).Lines / 25)
+    echo $lines
+    for ($i = 0; $i -lt $lines; $i += 1)
+    {
+        carbon-now $_.FullName -p csp-pic -t ($_.BaseName+$_.Extension+"--$i") -s ($i * 25) -e (($i * 25) + 24)
+    }
 }
 
 Get-ChildItem -Path "../src" -Filter "*.css" -Recurse -File | ForEach-Object {
-    carbon-now $_.FullName -p csp-pic -t ($_.BaseName+$_.Extension)
+    $lines = [Math]::Ceiling((Get-Content $_.FullName | Measure-Object -Line).Lines / 24)
+    echo $lines
+    for ($i = 0; $i -lt $lines; $i += 1)
+    {
+        carbon-now $_.FullName -p csp-pic -t ($_.BaseName+$_.Extension+"--$i") -s ($i * 25) -e (($i * 25) + 24)
+    }
 }
 
 $MarkdownFile = ""
 
 Get-Childitem -Path "." -Filter "*.png" -File | ForEach-Object {
-    $MarkdownFile = $MarkdownFile + [string]::Format($CodePicTemplate, $_.BaseName, $_.BaseName+$_.Extension)
+    $splitname = $_.BaseName.Split("--")
+    if ($splitname[2] -eq "0")
+    {
+        $MarkdownFile = $MarkdownFile + [string]::Format($CodePicTemplate, $splitname[0], $_.BaseName+$_.Extension)
+    }
+    else
+    {
+        $MarkdownFile = $MarkdownFile + [string]::Format($AdditionalPic, $_.BaseName+$_.Extension)
+    }
 }
 
 Out-File -FilePath .\Document.md -InputObject $MarkdownFile -Encoding utf8
