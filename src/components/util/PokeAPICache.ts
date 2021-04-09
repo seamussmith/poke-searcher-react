bfe511c5143a0df691ee61d947acf53d6ae7014c
seamussmith <student.141504@worcesterschools.net>
2021-04-09 11:03:41 -0400

    Export API endpoints

--- src/components/util/PokeAPICache.ts
+++ src/components/util/PokeAPICache.ts
@@ -4,19 +4,20 @@ export const PokemonCache: Record<string, Promise<IPokemon>> = {}
 export const SpeciesCache: Record<string, Promise<IPokemonSpecies>> = {}
 export const EvolutionCache: Record<string, Promise<IEvolutionChain>> = {}
 
-const API_ENDPOINT = "https://pokeapi.co/api/v2"
-const POKEMON_ENDPOINT = API_ENDPOINT+"/pokemon/"
+export const API_ENDPOINT = "https://pokeapi.co/api/v2"
+export const POKEMON_ENDPOINT = API_ENDPOINT+"/pokemon/"
+export const SPECIES_ENDPOINT = API_ENDPOINT+"/pokemon-species/"
+export const EVOTREE_ENDPOINT = API_ENDPOINT+"/evolution/"
+
 export async function GetPokemon(query: string)
 {
     return PokemonCache[query] ?? (PokemonCache[query] = fetch(query).then(blob => blob.json()))
 }
 
-const SPECIES_ENDPOINT = API_ENDPOINT+"/pokemon-species/"
 export async function GetPokemonSpecies(query: string)
 {
     return SpeciesCache[query] ?? (SpeciesCache[query] = fetch(query).then(blob => blob.json()))
 }
-const EVOTREE_ENDPOINT = API_ENDPOINT+"/evolution/"
 export async function GetEvolutionTree(query: string)
 {
     return EvolutionCache[query] ?? (EvolutionCache[query] = fetch(query).then(blob => blob.json()))
