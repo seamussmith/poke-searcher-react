f530f2fa9c634117f743f89986d6e7057e9d3a55
seamussmith <student.141504@worcesterschools.net>
2021-04-09 11:25:08 -0400

    Start on adding querying matches based on module

--- src/components/util/PokeAPICache.ts
+++ src/components/util/PokeAPICache.ts
@@ -1,8 +1,15 @@
-import { IPokemon, IPokemonSpecies, IEvolutionChain } from "pokeapi-typescript"
+import { IPokemon, IPokemonSpecies, IEvolutionChain, INamedApiResourceList } from "pokeapi-typescript"
 
 export const PokemonCache: Record<string, Promise<IPokemon>> = {}
 export const SpeciesCache: Record<string, Promise<IPokemonSpecies>> = {}
 export const EvolutionCache: Record<string, Promise<IEvolutionChain>> = {}
+export let PokeList: INamedApiResourceList<IPokemon>|null = null
+
+const indexListFetchJob =
+    (async function(){
+        PokeList = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=9999")
+                    .then(blob => blob.json())
+    })()
 
 export const API_ENDPOINT = "https://pokeapi.co/api/v2"
 export const POKEMON_ENDPOINT = API_ENDPOINT+"/pokemon/"
@@ -22,3 +29,16 @@ export async function GetEvolutionTree(query: string)
 {
     return EvolutionCache[query] ?? (EvolutionCache[query] = fetch(query).then(blob => blob.json()))
 }
+
+export async function MatchQuery(query: string, limit = -1)
+{
+    await indexListFetchJob // Wait until the index list is ready
+    let results = PokeList!.results.filter((i) => i.name.match(query) !== null)
+    if (limit > 0)
+    {
+        results = results.slice(0, limit)
+    }
+    return results
+}
+
+
