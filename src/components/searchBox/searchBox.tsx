e11f0ab94973911b9e4fed341689c400b5d777d6
seamussmith <student.141504@worcesterschools.net>
2021-04-13 17:59:25 -0400

    Set searchbox to pkmn name when pkmn selected

--- src/components/searchBox/searchBox.tsx
+++ src/components/searchBox/searchBox.tsx
@@ -1,21 +1,38 @@
 import React from 'react'
+import { InvokeQueryResult } from "../singletons/singletons"
+import { capitalize } from "../util/util"
 import './searchBox.css'
 
 type SearchBox_props = {
     keyUp: (query: string) => void
 }
-type SearchBox_state = {}
+type SearchBox_state = {
+    query: string
+}
 class SearchBox extends React.Component<SearchBox_props, SearchBox_state>
 {
     constructor(props: SearchBox_props)
     {
         super(props)
-        this.state = {}
-        this.keyUpInput = this.keyUpInput.bind(this)
+        this.state = {
+            query: ""
+        }
+        this.onChange = this.onChange.bind(this)
+        this.onQueryComplete = this.onQueryComplete.bind(this)
+        InvokeQueryResult.Subscribe((args) => this.onQueryComplete(args.pokemon.name))
     }
-    keyUpInput(event: React.KeyboardEvent<HTMLInputElement>)
+    onQueryComplete(pkmnName: string)
+    {
+        this.setState({
+            query: capitalize(pkmnName)
+        })
+    }
+    onChange(event: React.ChangeEvent<HTMLInputElement>)
     {
         this.props.keyUp(event.currentTarget.value)
+        this.setState({
+            query: event.currentTarget.value
+        })
     }
     render()
     {
@@ -27,7 +44,8 @@ class SearchBox extends React.Component<SearchBox_props, SearchBox_state>
                 className="search-box__input"
                 placeholder="Search for a Pokemon!"
                 spellCheck={false}
-                onKeyUp={this.keyUpInput}/>
+                value={this.state.query}
+                onChange={this.onChange}/>
             </div>
         )
     }
