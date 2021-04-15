405cbcb9ea84fabc15cfc141617685ecb4ecdbb7
seamussmith <student.141504@worcesterschools.net>
2021-04-15 10:45:57 -0400

    Convert SearchResult into functional component

--- src/components/searchResult/searchResult.tsx
+++ src/components/searchResult/searchResult.tsx
@@ -5,45 +5,33 @@ import '../typeColorClasses/typeColorClasses.css'
 import { IPokemon } from 'pokeapi-typescript'
 import { InvokeQueryResult } from "../singletons/singletons"
 
-type SearchResult_props = {
+function SearchResult(props: {
     pokeData: IPokemon
-    //detailHandler: (pkmnData: IPokemon) => void
-}
-class SearchResult extends React.Component<SearchResult_props, {}>
+})
 {
-    constructor(props: SearchResult_props)
-    {
-        super(props)
-        this.onClick = this.onClick.bind(this)
-    }
-    onClick()
-    {
+    const onClick = () => {
         window.scrollTo(0, 0)
-        InvokeQueryResult.Invoke({ pokemon: this.props.pokeData })
-        //this.props.detailHandler(this.props.pokeData)
+        InvokeQueryResult.Invoke({ pokemon: props.pokeData })
     }
-    render()
-    {
-        let pokeData = this.props.pokeData
-        return (
-            <div className='search-result' onClick={this.onClick}>
-                <h2 className='search-result-name'>
-                    <span className={pokeData.types[0].type.name}>{stylePokemonName(pokeData.name)}</span>
-                </h2>
-                <div>
-                    <img
-                    className="search-result__image nofilter"
-                    src={pokeData.sprites.front_default}
-                    alt={`${pokeData.name} sprite`}/>
-                </div>
-                <p className='search-result-types'>
-                    {pokeData.types.map((i) =>
-                        <span className={i.type.name}>{capitalize(i.type.name)} </span>
-                    )}
-                </p>
+    let pokeData = props.pokeData
+    return (
+        <div className='search-result' onClick={onClick}>
+            <h2 className='search-result-name'>
+                <span className={pokeData.types[0].type.name}>{stylePokemonName(pokeData.name)}</span>
+            </h2>
+            <div>
+                <img
+                className="search-result__image nofilter"
+                src={pokeData.sprites.front_default}
+                alt={`${pokeData.name} sprite`}/>
             </div>
-        )
-    }
+            <p className='search-result-types'>
+                {pokeData.types.map((i) =>
+                    <span className={i.type.name}>{capitalize(i.type.name)} </span>
+                )}
+            </p>
+        </div>
+    )
 }
 
 export default SearchResult
