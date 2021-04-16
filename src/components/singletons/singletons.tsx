6523c97ae0a5a4b2b816f9f0d4ae3f972eed64b1
seamussmith <student.141504@worcesterschools.net>
2021-04-16 11:19:17 -0400

    Depreciation warning

--- src/components/singletons/singletons.tsx
+++ src/components/singletons/singletons.tsx
@@ -1,6 +1,7 @@
 import { CustomEvent } from "../util/customEvent"
 import { IPokemon } from "pokeapi-typescript"
 
+// !!! Depreciated
 export const InvokeQueryResult = new CustomEvent<{
     pokemon: IPokemon
 }>()
