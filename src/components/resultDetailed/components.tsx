b7b2c36955a98749528612a01c89fcba0132e1a3
seamussmith <student.141504@worcesterschools.net>
2021-04-27 19:34:19 -0400

    Gender element

--- src/components/resultDetailed/components.tsx
+++ src/components/resultDetailed/components.tsx
@@ -238,3 +238,36 @@ export function TypeLabel(props: {
     else
         return <Type className={props.typeName}><span>{capitalize(props.typeName)}</span></Type>
 }
+
+// Gender
+
+const GenderColors: any = {
+    "male": "var(--male-stat-background)",
+    "female": "var(--female-stat-background)"
+}
+
+export const GenderLabel = styled.div<{
+    gender: string
+}>`
+    flex-grow: 1;
+    width: 40%;
+    height: 84px;
+    display: flex;
+    border-radius: var(--mini-divison-radius);
+    flex-direction: column;
+    justify-content: center;
+    background-color: ${({gender}) => GenderColors[gender] ?? "var(--layer-1)"};
+    box-shadow: 1px 2px 0px 0px var(--shadow-color);
+    margin: 5px;
+`
+
+export function Gender(props: {
+    gender: "male" | "female" | null
+})
+{
+    return (
+        <GenderLabel gender={props.gender ?? ""}>
+
+        </GenderLabel>
+    )
+}
