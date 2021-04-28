8a5173a5c936541ff4c56c9e4238d475781846e5
seamussmith <student.141504@worcesterschools.net>
2021-04-28 12:17:15 -0400

    Pomemon Grid

--- src/components/resultDetailed/components.tsx
+++ src/components/resultDetailed/components.tsx
@@ -342,3 +342,27 @@ export function InfoStat(props: {
         </InfoStatParent>
     )
 }
+
+export const PokemonGrid = styled.div`
+    background-color: var(--layer-0);
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 2fr));
+    grid-auto-rows: 150px;
+    justify-content: center;
+
+    gap: 10px;
+    padding: 5px;
+    margin: 10px;
+    border: 2px solid var(--layer-1);
+    overflow: auto;
+    max-height: 480px;
+    height: 520px;
+
+    &::-webkit-scrollbar {
+        width: 0.7em;
+    }
+
+    @media (max-width: 720px) {
+        grid-template-columns: 1fr;
+    }
+`
