a3a6b49dc93764e759138a6ff6d49ddcd3609f49
seamussmith <student.141504@worcesterschools.net>
2021-04-28 16:34:27 -0400

    Satisfy no description edge case

--- src/components/resultDetailed/components.tsx
+++ src/components/resultDetailed/components.tsx
@@ -413,14 +413,15 @@ export function Ability(props: {
     ability: IAbility
 })
 {
+    const abilityData = props.ability.effect_entries.filter((e) => e.language.name === "en")[0]
     return (
         <AbilityDiv>
             <AbilityName>
-                <Label2>{capitalize(props.ability.name)}</Label2>
+                <Label2>{capitalize(props.ability.name).replace("-", " ")}</Label2>
             </AbilityName>
             <AbilityDesc>
                 <p>
-                    {props.ability.effect_entries.filter((e) => e.language.name === "en")[0].short_effect}
+                    {abilityData?.short_effect ?? "[NO DESCRIPTION]" }
                 </p>
             </AbilityDesc>
         </AbilityDiv>
