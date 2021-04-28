7d2557af65c305e27166b1b04baa8e95f11eb9ea
seamussmith <student.141504@worcesterschools.net>
2021-04-28 12:01:18 -0400

    Add resultDetailed

--- src/components/resultDetailed/components.tsx
+++ src/components/resultDetailed/components.tsx
@@ -52,7 +52,6 @@ export const Division = styled.div<{
         grid-column: span 8 / auto;
         grid-row: span 8 / auto;
     }
-}  
 `
 
 export const EvenDivision = styled(Division)`
@@ -71,7 +70,21 @@ export const StatDiv = styled.div`
 `
 
 export const ResultDetailed = styled.div`
+    text-align: center;
+    display: grid;
+    grid-template-columns: repeat(8, 1fr); 
 
+    gap: 10px;
+
+    --pokemon-color: none;
+    width: 1000px;
+    transform-origin: top;
+    border-color: var(--pokemon-color);
+    background-color: var(--layer-1);
+    border-radius: var(--division-radius);
+    box-shadow: 1px 2px 2px 1px var(--shadow-color);
+    padding: 10px;
+    margin: 1% 0;
 `
 
 // Stat bars
