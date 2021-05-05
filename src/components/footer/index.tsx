b2953c9b85f6642be2a1d0d1dfbb7eecc97bab61
seamussmith <student.141504@worcesterschools.net>
2021-05-05 19:24:52 -0400

    Create Navbar styled component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './index.css'
 import '../navbar/navbar.css'
+import { MiniNavbar, Navbar, NavbarLink, NavbarListItem, NavbarUList } from "../navbar"
 
 function Footer()
 {
@@ -12,22 +13,22 @@ function Footer()
             <p className="footer__content">
                 A <a className="--animated-underline" href="https://github.com/seamussmith">Seamus Smith</a> website powered by PokeAPI.
             </p>
-            <nav className="navbar navbar--minature footer__content">
-                <ul className="navbar__ulist">
-                    <li className="navbar__ulist-item">
-                        <a className="navbar__link" href="https://github.com/seamussmith/poke-searcher-react">
-                        <i className="fab fa-github --force-inheritence"></i> github</a>
-                    </li>
-                    <li className="navbar__ulist-item">
-                        <a className="navbar__link" href="https://www.youtube.com/watch?v=G4z9yW72A1o">
-                        <i className="fas fa-angry --force-inheritence"></i> get rickrolled</a>
-                    </li>
-                    <li className="navbar__ulist-item">
-                        <a className="navbar__link" href="https://pokeapi.co/">
-                        <i className="fas fa-server --force-inheritence"></i> PokeAPI</a>
-                    </li>
-                </ul>
-            </nav>
+            <MiniNavbar>
+                <NavbarUList>
+                    <NavbarListItem>
+                        <NavbarLink href="https://github.com/seamussmith/poke-searcher-react">
+                        <i className="fab fa-github --force-inheritence"></i> github</NavbarLink>
+                    </NavbarListItem>
+                    <NavbarListItem>
+                        <NavbarLink href="https://www.youtube.com/watch?v=G4z9yW72A1o">
+                        <i className="fas fa-angry --force-inheritence"></i> get rickrolled</NavbarLink>
+                    </NavbarListItem>
+                    <NavbarListItem>
+                        <NavbarLink href="https://pokeapi.co/">
+                        <i className="fas fa-server --force-inheritence"></i> PokeAPI</NavbarLink>
+                    </NavbarListItem>
+                </NavbarUList>
+            </MiniNavbar>
         </footer>
     )
 }
