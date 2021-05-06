4460cdbc4a66547e035b82f52e8e0ad29c503f28
seamussmith <student.141504@worcesterschools.net>
2021-05-06 16:31:40 -0400

    Start working on moves

---
+++ src/components/resultDetailed/components/styleComponents/moves.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const MovesGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(500px, 2fr));
+`
+
+export const Move = styled.div`
+    background-color: var(--layer-3);
+`
