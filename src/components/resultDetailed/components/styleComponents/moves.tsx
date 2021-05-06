51290e8d074079bc311ae43fcb363ad84a193fff
seamussmith <student.141504@worcesterschools.net>
2021-05-06 17:06:14 -0400

    Styling

--- src/components/resultDetailed/components/styleComponents/moves.tsx
+++ src/components/resultDetailed/components/styleComponents/moves.tsx
@@ -5,12 +5,13 @@ export const MovesGrid = styled.div`
     grid-template-columns: repeat(auto-fit, minmax(500px, 2fr));
     height: 500px;
     overflow: auto;
-    background-color: var(--layer-0)
+    background-color: var(--layer-0);
 `
 
 export const Move = styled.div`
     background-color: var(--layer-1);
     margin: 5px;
-    border: 1px red solid;
+    box-shadow: 3px 3px 0px var(--shadow-color);
+    border-radius: 10px;
     padding: 0 10px;
 `
